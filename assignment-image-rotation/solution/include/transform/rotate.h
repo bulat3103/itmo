f720a4797a52eda1d9b71b35115679389182cc2b
Bulatka <45459817+bulat3103@users.noreply.github.com>
2022-10-21 17:54:33 +0300

    Delete assignment-image-rotation directory

--- assignment-image-rotation/solution/include/transform/rotate.h
+++
@@ -1,7 +0,0 @@
-#ifndef _ROTATE
-#define _ROTATE
-#include "../bmp/bmp.h"
-#include "../image/image.h"
-
-enum rotate_status rotate(const struct image oldImage, struct image* const newImage);
-#endif
